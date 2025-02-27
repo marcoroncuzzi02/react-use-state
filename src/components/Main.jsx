efc6c2f24b1c5f117ad4613731ffb569b7f87506
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-02-27 18:18:16 +0100

    END

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,9 @@ import languages from "./languages.js"
 import { useState } from "react"
 
 const Main = () => {
-
-    const [showText,setShowText] = useState('')
+    // questo usestate è per la visualizzazione al click del testo
+    const [showText,setShowText] = useState()
+    //questo use state è per il cambiamento di colore al click del bottone
     const [active,setActive] = useState()
 
     return(
@@ -14,6 +15,10 @@ const Main = () => {
             
                     <div className="container row buttons">
                         <button 
+                        //qui devo lavorare con una condizione sulla classe del bottone: 
+                        //se clicco il bottone vuol dire che il bottone selezionato (element.id) prende lo use state active, 
+                        //e se il bottone ha use state active allora prenderà la classe "btn-warning"
+                        //con i ":" imposto la logica "else", cioè dico ALTRIMENTI il bottone prende la classe btn-primary
                             className={active === element.id ? "btn btn-warning" : "btn btn-primary"}
                             key={element.id}
                             onClick={() =>{
